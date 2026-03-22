52c292fadaaafcdc7b5c82d4c6b82e79260fd48e
tielmess <trudy@trudymessingham.co.uk>
2026-03-22 03:25:19 +0000

    Add for Light-Dark mode

--- footer.php
+++ footer.php
@@ -3,5 +3,6 @@
             </footer>
         </footer>
     </div>
+    <?php wp_footer(); ?>
 </body>
 </html>
--- functions.php
+++ functions.php
@@ -3,6 +3,6 @@
 function wptm001_load_scripts() {
     // Enqueue the main stylesheet
     wp_enqueue_style( 'wptm001-style', get_stylesheet_uri(), array(), '1.0', 'all' );
-    wp_enqueue_script( 'wptm001-script', get_template_directory_uri() . '/js/app.js', array(), '1.0', true );
+    wp_enqueue_script( 'wptm001-script', get_template_directory_uri() . '/js/app.js', array(), '1.0', true ); /* true means load in footer */
 }
 add_action( 'wp_enqueue_scripts', 'wptm001_load_scripts' );
