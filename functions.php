f3f7cb17dd7f83c86ed927916781d953674b8d17
tielmess <trudy@trudymessingham.co.uk>
2026-03-24 02:09:17 +0000

    Minor updates

--- functions.php
+++ functions.php
@@ -6,3 +6,7 @@ function wptm001_load_scripts() {
     wp_enqueue_script( 'wptm001-script', get_template_directory_uri() . '/js/app.js', array(), '1.0', true ); /* true means load in footer */
 }
 add_action( 'wp_enqueue_scripts', 'wptm001_load_scripts' );
+
+
+
+
