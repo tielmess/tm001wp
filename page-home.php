ff5a589e181cc40164145430c8ebb5da06442704
tielmess <trudy@trudymessingham.co.uk>
2026-03-25 14:32:37 +0000

    Another quick page-home update to include title and content (rather than excerpt)

--- page-home.php
+++ page-home.php
@@ -21,8 +21,9 @@
                             while( have_posts() ) : the_post();
                         ?>
                             <article class="post card">
+                                <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
                                 <div class="entry-summary">
-                                    <?php the_excerpt(); ?>
+                                    <?php the_content() ?>
                                 </div>
                             </article>
                                 <?php
