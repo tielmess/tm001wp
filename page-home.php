f221904da0e639fcdc81aa560a8ffeaa3edb1e78
tielmess <trudy@trudymessingham.co.uk>
2026-03-25 14:46:15 +0000

    Sorting out page-home H2/menu copy

--- page-home.php
+++ page-home.php
@@ -21,7 +21,6 @@
                             while( have_posts() ) : the_post();
                         ?>
                             <article class="post card">
-                                <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
                                 <div class="entry-summary">
                                     <?php the_content() ?>
                                 </div>
