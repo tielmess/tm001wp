5155dad9791a78b2b0e6df440cb9e40acc03fe0b
tielmess <trudy@trudymessingham.co.uk>
2026-04-16 21:00:38 +0100

    Add 404page

--- 404.php
+++ 404.php
@@ -6,6 +6,25 @@
                 <h1>404 - Page Not Found</h1>
                 <p>Sorry, the page you are looking for does not exist. Please check the URL or return to the homepage.</p>
                 <a href="<?php echo home_url(); ?>" class="button">Go to Homepage</a>
+                <div>
+                    <h2>Latest Blog Posts</h2>
+                    <?php
+                    $recent_posts = new WP_Query( array(
+                        'posts_per_page' => 3,
+                        'post_status' => 'publish',
+                    ) );
+                    if ( $recent_posts->have_posts() ) :
+                        echo '<ul class="recent-posts">';
+                        while ( $recent_posts->have_posts() ) : $recent_posts->the_post();
+                            echo '<li><a href="' . get_permalink() . '">' . get_the_title() . '</a></li>';
+                        endwhile;
+                        echo '</ul>';
+                        wp_reset_postdata();
+                    else :
+                        echo '<p>No recent posts available.</p>';   
+                    endif;
+                    ?>
+                </div>
             </section><!-- .error-404 -->
         </main><!-- #main -->
     </div><!-- #nofind --> 
